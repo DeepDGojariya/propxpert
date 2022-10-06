062fbc66461be5c34a849b512fbd1bf2500276c9
Deep <deepgojariya123@gmail.com>
2022-10-06 15:08:35 +0530

    navbar done

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,26 +1,41 @@
 import React from 'react'
+import logo from "../static/download.png"
+import "./Navbar.css"
 export default function Navbar() {
     return (
         <>
-            <nav className="navbar navbar-light bg-light">
+
+
+            <nav className="navbar" style={{ backgroundColor: "#1B2430" }}>
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="/">
-                        <img src="/docs/5.0/assets/brand/bootstrap-logo.svg" alt="" width="30" height="24" className="d-inline-block align-text-top" />
+                    <a className="navbar-brand nav-item" href="/">
+                        {/* <img src={logo} alt="" width="20" height="20" className="d-inline-block align-text-top" style={{borderRadius:"50%"}} /> */}
                         Bootstrap
                     </a>
-                    {/* <<div className="collapse navbar-collapse" id="navbarNav"> */}
-                        <ul className="d-flex" style={{listStyle:"none"}}>
-                            
-                            <li className="nav-item">
-                                <a className="nav-link" href="/">Login</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link disabled" href="/" tabindex="-1" aria-disabled="true">Post property</a>
-                            </li>
-                        </ul>
-                    </div>
-                {/* </div> */}
+
+                    <ul className="d-flex" style={{ listStyle: "none" }}>
+
+                        <li className="nav-item">
+                            <a className="nav-link" href="/">Login</a>
+                        </li>
+                        <li className="nav-item">
+                            <button className="nav-link btn" href="/" style={{ backgroundColor: "#51557E" }}>Post property</button>
+                        </li>
+                    </ul>
+                </div>
+
+
             </nav>
+            <div className="container-fluid-2" style={{ backgroundColor: "#51557E" }}>
+                <ul className="d-flex" style={{ listStyle: "none" }}>
+                    <li className="nav-item" style={{ backgroundColor: "#51557E" }}>
+                        <a className="nav-link" href="/">Buy</a>
+                    </li>
+                    <li className="nav-item-2" style={{ backgroundColor: "#51557E" }}>
+                        <a className="nav-link" href="/">Sell</a>
+                    </li>
+                </ul>
+            </div>
         </>
     )
 }
